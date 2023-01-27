83a68584b00a0f6ebd7e9ed97d417467e10bf344
RahulPoddar1602 <rahul1999p@gmail.com>
2023-01-27 13:09:06 +0530

    third commit

---
+++ src/screens/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <Navbar />
+        </div>
+        <div>
+            body
+        </div>
+        <div>
+            <Footer />
+        </div>
+    </div>
+
+  )
+}
+
+export default Home
